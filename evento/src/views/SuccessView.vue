4a5d3d49088b1be8680013f6dfa4f628323c3413
Willian Eliseu da Silva <w.eliseu@gmail.com>
2025-09-02 17:55:43 -0300

    Update SuccessView.vue

--- evento/src/views/SuccessView.vue
+++ evento/src/views/SuccessView.vue
@@ -12,12 +12,35 @@ sessionStorage.setItem('enable', 1)
           <h2 class="text-center text-principal">
             Curso Revisão Pré-Prova
           </h2>
-          <p class="fs-5 text-center text-principal">
-            O evento será transmitido ao vivo nos dias 07, 08 e 09 de novembro de 2025
-          </p>
           <p class="pt-3 fs-4 text-center">
             <b>Seu pagamento foi realizado com sucesso!</b>
           </p>
+          <p class="fs-5 text-principal text-center">
+            O curso será realizado nos dias 07, 08 e 09 de novembro de 2025.
+          </p>
+          <div class="row justify-content-center">
+            <div class="col-auto">
+              <p class="fw-semibold fs-5 mb-0">
+                Presencial:
+              </p>
+              <ul class="mb-3">
+                <li>Hotel Íbis Porto São João</li>
+                <li>Rua Dr. Plácido da Costa, 4200-450 Porto</li>
+              </ul>
+              <p class="fw-semibold fs-5 mb-0">
+                Ao vivo pela internet:
+              </p>
+              <ul class="mb-3">
+                <li>Acesse essa plataforma</li>
+              </ul>
+              <p class="fw-semibold fs-5 mb-0">
+                Aulas gravadas após o curso:
+              </p>
+              <ul class="mb-3">
+                <li>Acesse essa plataforma</li>
+              </ul>
+            </div>
+          </div>
         </article>
       </section>
     </main>
